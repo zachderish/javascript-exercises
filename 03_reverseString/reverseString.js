cf9ca1ffc1b528420206419507ae38c03808d338
Zach Derish <zderish@gmail.com>
2023-11-01 17:05:21 -0400

    Complete exercises 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+function reverseString(str) {
+    reversedString = "";
+    let i = str.length - 1;
+    while (i >= 0) {
+        reversedString += str[i]
+        i--;
+    }
+    return reversedString;
 
-};
+}
 
 // Do not edit below this line
 module.exports = reverseString;
