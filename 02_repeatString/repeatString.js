df80f8a8d5c1960d1d5bdef1b98f40a9771afb0b
Zach Derish <zderish@gmail.com>
2023-11-06 18:37:36 -0500

    Complete exercise 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+function repeatString(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
-};
+    returnString = "";
+
+    for(let i = 0; i < num; i++) {
+        returnString += string;
+    }
+
+    return returnString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    let array = arguments[0].slice();
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (arguments[i] === array[j]) {
+                array.splice(j, 1);
+            }
+        }
+    }
+    console.log(array)
+    return array;
 };
 
 // Do not edit below this line
