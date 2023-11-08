913b2ee7fe8541c9b532f7537491a7f00c7e722d
Zach Derish <zderish@gmail.com>
2023-11-08 17:44:12 -0500

    Complete challenges 05 and 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
 const sumAll = function() {
+    start = arguments[0];
+    end = arguments[1];
+    if (end < start) {
+        start = arguments[1];
+        end = arguments[0];
+    }
+    console.log(start, end);
+    sum = 0;
+    if (typeof(end) != "number" || typeof(start) != "number" || start < 0 || end < 0) {
+        return 'ERROR';
+    }
+
+    while (start <= end) {
+        sum += start;
+        start++;
+    }
+    return sum;
 
 };
 
