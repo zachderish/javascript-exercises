23c278766a334f96c5b17911716542e562088c40
Zach Derish <zderish@gmail.com>
2023-12-19 14:20:15 -0500

    Finish exercises up to 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
 const convertToCelsius = function() {
+  let FTemp = arguments[0];
+  const FACTOR = 5/9;
+  let CTemp = (FTemp - 32) * FACTOR;
+  CTemp = +CTemp.toFixed(1);
+  return CTemp;
 };
 
 const convertToFahrenheit = function() {
+  let CTemp = arguments[0];
+  const FACTOR = 9/5;
+  let FTemp = (CTemp * FACTOR) + 32;
+  FTemp = +FTemp.toFixed(1);
+  return FTemp;
 };
 
 // Do not edit below this line
